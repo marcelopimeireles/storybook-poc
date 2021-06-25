a52947d1bbaa6f73477fa9443c509d89ff440a53
Marcelo Meireles <marcelo.meireles@eae.pt>
2021-06-25 15:35:41 +0100

    primary button with knobs

--- stories/Components/Button.stories.ts
+++ stories/Components/Button.stories.ts
@@ -1,22 +1,27 @@
 import {html} from 'lit-html';
 import {TemplateResult} from 'lit-element';
+import { withKnobs, select, boolean, text } from '@storybook/addon-knobs';
+import { ColorsEnum, SizesEnum, TypesEnum } from 'sdb-dd-webcomponents';
 import 'sdb-dd-webcomponents';
 
 export default {
-	title: 'Components/Primary Button',
+  title: "Components/Primary Button",
+  component: "PrimaryButton",
+  decorators: [withKnobs],
 };
 
-export const Desktop: () => TemplateResult = () => {
+export const Example: () => TemplateResult = () => {
 	return html`
 		<div>
-			<primary-button size="desktop" type="button" color="red">Desktop</primary-button>
-		</div>
-	`;
-};
-export const Mobile: () => TemplateResult = () => {
-	return html`
-		<div>
-			<primary-button size="mobile" type="button" color="red">Mobile</primary-button>
+    <primary-button
+        size=${select("Size", SizesEnum, SizesEnum.MOBILE)}
+        type=${select("Type", TypesEnum, TypesEnum.BUTTON)}
+        color=${select("Color", ColorsEnum, ColorsEnum.RED)}
+        ?disabled=${boolean("Disabled", false)}
+        ?loading=${boolean("Loading", false)}
+        ?rounded=${boolean("Rounded", false)}
+        >${text("Text", "Awesome Button")}
+    </primary-button>
 		</div>
 	`;
 };
